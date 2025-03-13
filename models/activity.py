85665d37680ca57c526e1c0eeaa828111af9e6da
Gabriel Silva <gabrielmsilva4@ua.pt>
2025-03-13 14:41:28 +0000

    add Activity image

--- models/activity.py
+++ models/activity.py
@@ -2,7 +2,11 @@ from dependencies.database import Base
 from sqlalchemy import  Column, \
                         Integer, \
                         String, \
-                        Boolean
+                        Boolean, \
+                        Time, \
+                        ForeignKey
+from sqlalchemy_imageattach.entity import Image, image_attachment
+from sqlalchemy.orm import relationship
 
 class Activity(Base):
     __tablename__ = "activities"
@@ -10,9 +14,15 @@ class Activity(Base):
     id = Column(Integer, primary_key=True, index=True)
     name = Column(String, unique=True, index=True, nullable=False)
     description = Column(String, nullable=False)
-    image = Column(String, nullable=False)
-    date = Column(String, nullable=False)
-    duration = Column(String, nullable=False)
+    image = image_attachment("ActivityImage")
+    date = Column(Time, nullable=True)
+    duration = Column(Integer, nullable=True)
+
+class ActivityImage(Base, Image):
+    __tablename__ = "activity_images"
+
+    activity_id = Column(Integer, ForeignKey("activities.id"), primary_key=True)
+    activity = relationship("Activity")
 
 class Talk(Activity):
     __tablename__ = "talks"
