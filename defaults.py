a8fdab1249a1918b2ea5a780af10701a1320dffd
MariaLinhares <marialuis.linhares10@gmail.com>
2025-05-05 22:14:36 +0100

    edit defaults

--- defaults.py
+++ defaults.py
@@ -12,7 +12,7 @@ from schemas.ui.components.text import Text
 from schemas.ui.components.button import Button
 from schemas.ui.components.location import Location
 from schemas.ui.components.video import Video
-from schemas.ui.components.activicties import Activities
+from core.schemas.ui.components.activities import Activities
 from services.component_registry import ComponentRegistry
 from services.media import MediaService
 from repository.media import LocalMediaRepo
