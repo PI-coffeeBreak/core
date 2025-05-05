3658df3f47254383f1f60cdc22f9aa89fe7b9845
Gabriel Silva <gabrielmsilva4@ua.pt>
2025-05-05 02:04:46 +0100

    input settings changes

--- schemas/plugin_setting.py
+++ schemas/plugin_setting.py
@@ -1,38 +1,62 @@
-from typing import List, Union
+from typing import List, Union, Optional
 from pydantic import BaseModel
 
 class SelectorInput(BaseModel):
-    type: str
+    type: str = "selector"
     title: str
     description: str
     options: List[str]
 
 class TextInput(BaseModel):
-    type: str
+    type: str = "text"
     title: str
 
 class ShortTextInput(BaseModel):
-    type: str
+    type: str = "shortText"
     title: str
+
+class ToggleInput(BaseModel):
+    type: str = "toggle"
+    title: str
+    description: Optional[str] = None
+    text: Optional[str] = None
+    default: bool = False
+    required: bool = False
 
 class CheckboxInput(BaseModel):
-    type: str
-    kind: List[str]
+    type: str = "checkbox"
     title: str
-    options: List[str]
+    description: Optional[str] = None
+    options: List[str]  # Options to select from
+    default: Optional[List[str]] = None  # Default selected options
+
+class RadioInput(BaseModel):
+    type: str = "radio"
+    title: str
+    description: Optional[str] = None
+    options: List[str]  # Options to select from
+    default: Optional[str] = None
+    required: bool = False
 
 class ComposedTextInput(BaseModel):
-    type: str
+    type: str = "composedText"
     title: str
     name: str
     description: str
 
+class NumberInput(BaseModel):
+    type: str = "number"
+    title: str
+    description: str
+    min: int
+    max: int
+    step: int
+    default: int
+
 class PluginSetting(BaseModel):
     title: str
     description: str
-    inputs: List[Union[SelectorInput, TextInput, ShortTextInput, CheckboxInput, ComposedTextInput]]
-
-
+    inputs: List[Union[SelectorInput, TextInput, ShortTextInput, CheckboxInput, ToggleInput, RadioInput, ComposedTextInput, NumberInput]]
 
 # Example usage
 example_json = {
@@ -55,9 +79,27 @@ example_json = {
         },
         {
             "type": "checkbox",
-            "kind": ["multiple", "single", "toggle"],
             "title": "title",
-            "options": ["op1", "op2", "op3"]
+            "description": "description",
+            "options": ["op1", "op2", "op3"],
+            "multiple": True,
+            "default": ["op1"],
+            "required": False
+        },
+        {
+            "type": "toggle",
+            "title": "title",
+            "description": "description",
+            "default": True,
+            "required": False
+        },
+        {
+            "type": "radio",
+            "title": "title",
+            "description": "description",
+            "options": ["op1", "op2", "op3"],
+            "default": "op1",
+            "required": False
         },
         {
             "type": "composedText",
